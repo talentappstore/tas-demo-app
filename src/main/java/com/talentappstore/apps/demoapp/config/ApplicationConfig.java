789b4ab5cd0b2062f25a4d58046dafd4738f8e8e
Dion Short <dion.short@aotal.com>
2018-09-18 14:57:41 +1200

    Update javadoc and remove misc unnecessary bits and pieces.

--- src/main/java/com/talentappstore/apps/demoapp/config/ApplicationConfig.java
+++ src/main/java/com/talentappstore/apps/demoapp/config/ApplicationConfig.java
@@ -2,10 +2,8 @@ package com.talentappstore.apps.demoapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
 public class ApplicationConfig
 {
 
